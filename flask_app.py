90d16af95d005e42e2b705bd171f7466a70f3025
arloft <get@aaronarlof.me>
2018-10-07 11:53:07 -0400

    added another route to the setup file

--- flask_app.py
+++ flask_app.py
@@ -2,46 +2,52 @@
 ## A very simple Flask Hello World app for you to get started with...
 ## --- below is the default generated app from pythonanywhere's Flask install
 
-# from flask import Flask
-
-# app = Flask(__name__)
-
-# @app.route('/')
-# def hello_world():
-#    return 'Hello from Flask! <a href="./hello">Say hello again</a>'
-
-
-import os
-
 from flask import Flask
+
+app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
     return 'Hello from Flask! <a href="./hello">Say hello again</a>'
 
+@app.route('/hello')
+def hello():
+    return '''
+        <p>say hello again</p>
+        <p><a href="/"></a></p>
+        '''
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskrsqlite'),
-        )
-    if test_config is None:
-        # load the instance config, if it exists, when NOT testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
-
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-        except OSERROR:
-            pass
-    # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
-
-    return app
+##import os
+##
+##from flask import Flask
+##
+##@app.route('/')
+##def hello_world():
+##    return 'Hello from Flask! <a href="./hello">Say hello again</a>'
+##
+##
+##def create_app(test_config=None):
+##    # create and configure the app
+##    app = Flask(__name__, instance_relative_config=True)
+##    app.config.from_mapping(
+##        SECRET_KEY='dev',
+##        DATABASE=os.path.join(app.instance_path, 'flaskrsqlite'),
+##        )
+##    if test_config is None:
+##        # load the instance config, if it exists, when NOT testing
+##        app.config.from_pyfile('config.py', silent=True)
+##    else:
+##        # load the test config if passed in
+##        app.config.from_mapping(test_config)
+##
+##    # ensure the instance folder exists
+##    try:
+##        os.makedirs(app.instance_path)
+##        except OSERROR:
+##            pass
+##    # a simple page that says hello
+##    @app.route('/hello')
+##    def hello():
+##        return 'Hello, World!'
+##
+##    return app
